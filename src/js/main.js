60d09d05dd2997bc8f84412ce1918b1edc75af18
Vincent Valdez <vmdvaldez@hotmail.com>
2023-10-31 18:35:06 -0400

    Add Subtask Addition

--- src/js/main.js
+++ src/js/main.js
@@ -53,6 +53,10 @@ class Task{
 
     // subTask setters and getter and helpers
     get subTasks(){return this.#subTasks};
+    get lastSubTask(){
+        const st = Object.keys(this.#subTasks);
+        return this.#subTasks[st[st.length-1]];
+    };
 
     addSubTask(taskName) {
         this.#subTasks[`${this.#subTaskId}`] = new SubTask(taskName, this.#subTaskId);
@@ -121,9 +125,47 @@ const navBarManager = (() =>{
 
 const taskContentManager = (() =>{
     const divTask = document.getElementById('task');
+    const addSubTask = (() => {
+        const li = document.createElement('li');
+        li.innerText = 'Add SubTask +';
+        li.id = 'add-subtask';
+
+        const form = createForm();
+
+        form.addEventListener('submit', (e)=>{
+            e.preventDefault();
+            const task = document.querySelector('.active').dataset.taskid;
+            const subTaskName = document.querySelector('#form-addsubtask input').value;
+            const x = taskList[task];
+            x.addSubTask(subTaskName);
+
+            const st = x.lastSubTask;
+    
+            form.reset();
+            form.remove();
+            createNewSubTask(st.name, st.id);
+            showAddSubTask();
+
+        });
+
+        li.addEventListener('click', ()=>{
+            hideAddSubTask();
+            insertBeforeAddSubTask(form);
+        });
+
+        return li;
+    })();
+
+    function createNewSubTask(text, id){
+        const li = document.createElement('li');
+        li.classList.add('subtask');
+        li.dataset.subtaskid = id;
+        li.innerText = text;
+        insertBeforeAddSubTask(li);
+        return li;
+    };
 
     function clearTaskWindow(){divTask.innerText=''};
-
     function displayTask(task){
         const title = document.createElement('div');
         title.classList.add('task-title');
@@ -131,12 +173,14 @@ const taskContentManager = (() =>{
 
 
         const ul = document.createElement('ul');
+        ul.id = 'subtask-list';
 
         const li = (t)=>{
             const li = document.createElement('li');
             console.log(t);
+            li.classList.add('subtask');
             li.innerText = t.name;
-            li.dataset.subclassid = t.id;
+            li.dataset.subtaskid = t.id;
             return li;
         };
         
@@ -145,11 +189,57 @@ const taskContentManager = (() =>{
             ul.appendChild(li(subTasks[t]));
         }
 
+        ul.appendChild(addSubTask);
+
         divTask.appendChild(title);
         divTask.appendChild(ul);
+    };
+
+    function createForm(){
+        const form = document.createElement('form');
+        form.id = 'form-addsubtask';
+
+        const div = document.createElement('div');
+        div.classList.add('form-buttons');
+        
+        const btn = (text, type) =>{
+           const b = document.createElement('button');
+           b.innerText = text;
+           b.type = type;
+           return b;
+        } 
+
+        const addBtn = btn('ADD', 'submit');
+        const cancelBtn = btn('CANCEL', 'button');
+
+        const input = document.createElement('input');
+        input.minLength = 1;
+        input.maxLength = 15;
+        input.placeholder = "Subtask Name";
+        input.required = true;
+
+        div.appendChild(addBtn);
+        div.appendChild(cancelBtn);
+        form.appendChild(input);
+        form.appendChild(div);
+
+        cancelBtn.addEventListener('click', ()=>{
+            form.remove();
+            addSubTask.style.display = 'list-item';
+        });
+
+        return form;
+    }  
+
+    function hideAddSubTask(){addSubTask.style.display = 'none'};
+    function showAddSubTask(){addSubTask.style.display = 'list-item'};
+    function insertBeforeAddSubTask(elem){
+        const subTaskList = document.getElementById('subtask-list');
+        subTaskList.insertBefore(elem, addSubTask);
     }
 
-    return {displayTask, clearTaskWindow};
+    return {displayTask, clearTaskWindow, hideAddSubTask, showAddSubTask, 
+        insertBeforeAddSubTask};
 })();
 
 const addTask = document.getElementById('add-task');
@@ -166,10 +256,8 @@ addTask.addEventListener('click', () => {
         t.addSubTask('aasdasd512');
         t.addSubTask('aasdasd12312');
 
-        //Backlogic TODO:
         taskList[t.id] = t;
 
-        //DOMlogic
         form.remove();
         const task = navBarManager.createNewTask(t.name, t.id);
         navBarManager.showAddTask();
@@ -183,6 +271,8 @@ addTask.addEventListener('click', () => {
             taskContentManager.displayTask(t);
             task.classList.toggle('active');
         });
+
+
     });
     navBarManager.insertBeforeAddTask(form);
 });
